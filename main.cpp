1bf63aed0949c2b7f4bc92b43bfd7c4b6e4f4513
GerardoRamirez-Maldonado <109577066+GerardoRamirez-Maldonado@users.noreply.github.com>
2022-07-18 23:31:58 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Gerardo Ramirez-Maldonado
+ * Created on June 30, 2022, 8:37 PM
+ * Purpose: C++ template - To be used in all future Assignments 
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Mathematical/Physics/Conversions, Higher dimensioned arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map inputs to outputs -> The Process
+    
+    //Display Results
+    
+    //Exit stage right
+    return 0;
+}
+
